3204eab65b9d23882bdcfa954112b454f2150abe
kimjoohye <joohea5943@naver.com>
2022-06-07 18:54:29 +0900

    spring_20220607

---
+++ workspace/S_SH09_02/src/main/java/com/pcwk/ehr/UserService.java
@@ -0,0 +1,13 @@
+package com.pcwk.ehr;
+
+import java.sql.SQLException;
+
+public interface UserService {
+	
+	/**
+	 * 등업 기능
+	 * @throws SQLException
+	 */
+	public void upgradeLevels() throws SQLException;
+	
+}
